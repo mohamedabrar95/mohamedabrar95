33b8e6a09ba5074df1715678132f4a2affb42277
mohamedabrar95 <mohamedabrar95@gmail.com>
2022-04-08 11:40:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mohamedabrar95
---
+++ time_till_deadline.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+user_input = input("enter your goal and the deadline with the colon separation\n")
+list_of_input = user_input.split(":")
+goal = list_of_input[0]
+deadline = list_of_input[1]
+deadline_date = datetime.strptime(deadline, "%d.%m.%Y")
+today = datetime.today()
+days_left = deadline_date - today
+print(f"the number of days left to complete my goal is {days_left.days} days")
+
