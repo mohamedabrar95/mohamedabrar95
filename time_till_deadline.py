0b5606adbb006d8b7b880177ae7faa4e14400cd0
mohamedabrar95 <102568475+mohamedabrar95@users.noreply.github.com>
2022-04-08 11:50:12 +0530

    adding feature

--- time_till_deadline.py
+++ time_till_deadline.py
@@ -7,4 +7,4 @@ deadline_date = datetime.strptime(deadline, "%d.%m.%Y")
 today = datetime.today()
 days_left = deadline_date - today
 print(f"the number of days left to complete my goal is {days_left.days} days")
-
+#adding some more feature
